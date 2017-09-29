55d2433dd6a82b572207f1b0507703f57093282f
Baha <ukonat@gmail.com>
2017-09-29 17:49:17 +0200

    Counts number of times eaten

--- philo.erl
+++ philo.erl
@@ -9,29 +9,30 @@ start(WaiterPid) ->
     {seated, PhiloId} ->
       io:fwrite("Philo " ++ integer_to_list(PhiloId) ++ " has been seated~n")
   end,
-  loop(WaiterPid, PhiloId).
+  loop(WaiterPid, PhiloId, 0).
 
-loop(WaiterPid, PhiloId) ->
+loop(WaiterPid, PhiloId, NumEaten) ->
   think(PhiloId),
-  hungry(WaiterPid, PhiloId),
-  loop(WaiterPid, PhiloId).
+  HasEaten = hungry(WaiterPid, PhiloId, NumEaten),
+  loop(WaiterPid, PhiloId, NumEaten + HasEaten).
 
 think(PhiloId) ->
   io:fwrite("Philo " ++ integer_to_list(PhiloId) ++ " is thinking~n"),
   ThinkTime = rand:uniform(?THINK_FACTOR),
   timer:sleep(ThinkTime).
 
-hungry(WaiterPid, PhiloId) ->
+hungry(WaiterPid, PhiloId, NumEaten) ->
   io:fwrite("Philo " ++ integer_to_list(PhiloId) ++ " is hungry~n"),
   WaiterPid ! {hungry, self()},
   receive
     think ->
-      ok;
+      0;
     eat   ->
       eat(PhiloId),
-      WaiterPid ! {eaten, self()};
+      WaiterPid ! {eaten, self()},
+      1;
     close ->
-      io:fwrite("Philo " ++ integer_to_list(PhiloId) ++ " leaves~n"),
+      io:fwrite("Philo " ++ integer_to_list(PhiloId) ++ " leaves, has eaten ~p times~n", [NumEaten]),
       exit(normal)
   end.
 
